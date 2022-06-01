6423d9537df8c28a807c56c913a56bf4941c72f8
bala5698 <189p5a0208@gmail.com>
2022-06-01 20:12:43 +0530

    commint the file

--- TestGit/src/com/browser/TestMethod.java
+++ TestGit/src/com/browser/TestMethod.java
@@ -4,7 +4,9 @@ public class TestMethod {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ System.out.println("BALAMURALIKRISHNA");
+ System.out.println("ELECTRICALANDELELECTRIONG");
+ System.out.println("ANDHRAPRADESH");
 	}
 
 }
